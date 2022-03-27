1d4a8d6e2e33e3725b98a0ddb59e307fdb4857f1
unknown <ans9971@naver.com>
2022-03-27 17:35:41 +0900

    로그인요청하는 fetch생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -7,6 +7,20 @@ console.log(id);
 loginBtn.addEventListener("click",login);
 
 function login(){
-    console.log(id.value);
+    const req ={
+        id : id.value,
+        psword:psword.value,
+    };
+    //첫번재 경로 경로로 전달 두번째파라미터로 전달할 데이터를 보내줌 오브젝트형태로
+    //json 형태로 전달
+    fetch("/login", {
+        //body를 통해서 데이터 전달하려ㄴ면 post 메서드ㅏ로 전달해야됨 rest
+        method:"POST",
+        //내가보내는게 json파일이다 라는걸 알려줘야됨 데이터타입
+        headers:{
+            "Content-Type":"application/json",
+        },
+        body : JSON.stringify(req),
+    })
 }
  
